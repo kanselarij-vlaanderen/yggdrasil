d35b67e4c0b2434657448702e3a30ffabce9f111
Michaël Dierick <michael.dierick@redpencil.io>
2022-06-13 14:48:47 +0200

    Extend delta config for decision-activity

--- model.js
+++ model.js
@@ -16,6 +16,7 @@ const typeUris = [
   { key: 'subcase', uri: 'dossier:Procedurestap' },
   { key: 'case', uri: 'dossier:Dossier' },
   { key: 'agendaitemTreatment', uri: 'besluit:BehandelingVanAgendapunt' },
+  { key: 'decisionActivity', uri: 'besluitvorming:Beslissingsactiviteit' },
   { key: 'newsitem', uri: 'besluitvorming:NieuwsbriefInfo' },
   { key: 'piece', uri: 'dossier:Stuk' },
   { key: 'documentContainer', uri: 'dossier:Serie' }
@@ -45,6 +46,9 @@ const pathsFromAgenda = {
   agendaitemTreatment: [
     { source: 'agendaitem', predicate: '^besluitvorming:heeftOnderwerp' }
   ],
+  decisionActivity: [
+    { source: 'agendaitemTreatment', predicate: 'besluitvorming:heeftBeslissing' }
+  ],
   newsitem: [
     { source: 'agendaitemTreatment', predicate: 'prov:generated' },
     { source: 'meeting', predicate: 'ext:algemeneNieuwsbrief' }
@@ -52,7 +56,7 @@ const pathsFromAgenda = {
   piece: [
     { source: 'agendaitem', predicate: 'besluitvorming:geagendeerdStuk' },
     { source: 'agendaitem', predicate: 'ext:bevatReedsBezorgdAgendapuntDocumentversie' },
-    { source: 'agendaitemTreatment', predicate: 'ext:documentenVoorBeslissing' },
+    { source: 'decisionActivity', predicate: 'ext:documentenVoorBeslissing' },
     { source: 'agendaitemTreatment', predicate: 'besluitvorming:genereertVerslag' },
     { source: 'newsitem', predicate: 'ext:documentenVoorPublicatie' },
     { source: 'submissionActivity', predicate: 'prov:generated' },
