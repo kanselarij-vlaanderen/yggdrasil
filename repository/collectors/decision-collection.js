5dc27d12583e54679abe5feec64ab84319af6e5b
Erika Pauwels <erika.pauwels@gmail.com>
2022-11-19 15:22:54 +0100

    Restore missing prefix in SPARQL query

--- repository/collectors/decision-collection.js
+++ repository/collectors/decision-collection.js
@@ -23,6 +23,7 @@ async function collectReleasedAgendaitemTreatments(distributor) {
   const path = properties.map(prop => prop.join(' / ')).map(path => `( ${path} )`).join(' | ');
 
   const relatedQuery = `
+      PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
       PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
       PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
       PREFIX prov: <http://www.w3.org/ns/prov#>
