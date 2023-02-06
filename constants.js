19fdae296169dc55e76d17d7324ea5a72f505c2a
Sven Valenberghs <sven.valenberghs@faros.be>
2023-02-06 15:59:12 +0100

    KAS-3775 change design agenda status URI to themis URI

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ const CABINET_GRAPH = 'http://mu.semte.ch/graphs/organizations/intern-regering';
 const GOVERNMENT_GRAPH = 'http://mu.semte.ch/graphs/organizations/intern-overheid';
 
 const AGENDA_TYPE = 'http://data.vlaanderen.be/ns/besluitvorming#Agenda';
-const DESIGN_AGENDA_STATUS = 'http://kanselarij.vo.data.gift/id/agendastatus/2735d084-63d1-499f-86f4-9b69eb33727f';
+const DESIGN_AGENDA_STATUS = 'http://themis.vlaanderen.be/id/concept/agenda-status/b3d8a99b-0a7e-419e-8474-4b508fa7ab91';
 
 const ACCESS_LEVEL_SECRETARY = 'http://themis.vlaanderen.be/id/concept/toegangsniveau/66804c35-4652-4ff4-b927-16982a3b6de8'; // intern secretarie
 const ACCESS_LEVEL_CABINET = 'http://themis.vlaanderen.be/id/concept/toegangsniveau/13ae94b0-6188-49df-8ecd-4c4a17511d6d'; // intern regering
