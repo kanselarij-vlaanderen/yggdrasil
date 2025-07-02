eb994096d60428c74b27d23682af62ea73773b07
Erika Pauwels <erika.pauwels@gmail.com>
2025-07-02 14:15:00 +0200

    Collect physical files after collecting virtual and derived files

--- repository/distributors/minister-distributor.js
+++ repository/distributors/minister-distributor.js
@@ -97,12 +97,12 @@ export default class MinisterDistributor extends Distributor {
         await this.collectVisibleFiles();
       }, this.constructor.name);
 
-      await runStage('Collect physical files', async () => {
-        await collectPhysicalFiles(this);
-      }, this.constructor.name);
-
       await runStage('Collect derived files', async() => {
         await collectDerivedFiles(this);
+      }, this.constructor.name);
+
+      await runStage('Collect physical files', async () => {
+        await collectPhysicalFiles(this);
       }, this.constructor.name);
 
       await runStage('Collect publication-flows', async() => {
